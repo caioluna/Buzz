cf155dee5b93b8bf6c737b9cb98e1b24e98e241b
Caio Luna <ccluna@gmail.com>
2025-10-14 19:55:46 -0300

    feat: start project - router layer

---
+++ Buzz/Scenes/NewsList/NewsListRouter.swift
@@ -0,0 +1,16 @@
+//
+//  NewsListRouter.swift
+//  Buzz
+//
+//  Created by Caio Luna on 14/10/25.
+//
+
+import UIKit
+
+class NewsListRouter {
+	static func createInitialViewController() -> UIViewController {
+		let newsListViewController = NewsListViewController()
+		let navigationController = UINavigationController(rootViewController: newsListViewController)
+		return navigationController
+	}
+}
